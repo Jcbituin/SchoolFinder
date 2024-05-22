4b0c7605198c2c48e9cb87f5b23c7e1cec3aff5d
Jcbituin <150402651+Jcbituin@users.noreply.github.com>
2024-05-22 15:42:10 +0800

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -72,6 +72,43 @@ schools = {
         - Bachelor of Science in Information System (BSIS)
         - Bachelor of Science in Information Technology (BSinfoTech)
         - Bachelor of Science in Computer Science (BSCS)
+        
+        - BACHELOR IN AUTOMOTIVE ENGINEERING TECHNOLOGY (BAET)
+        - BACHELOR IN ELECTRICAL ENGINEERING TECHNOL0GY (BEET)
+        - BACHELOR IN ELECTRONICS ENGINEERING TECHNOLOGY (BEXET)
+        - BACHELOR IN MECHANICAL ENGINEERING TECHNOLOGY (BMET)
+        
+        CONCENTRATION IN: 
+        - MECHANICAL TECHNOLOGY (BMET- MT)
+        - REFRIGERATION AND AIR-CONDITIONING TECHNOLoGY (BMET-RAC)
+            
+        Bachelor of Science in Industrial Technology (BSIT) Major in:
+        - Architectural Drafting Technology (BSIT-ADT)
+        - Automotive Technology (BSIT-AT)
+        - Electricol Technology (BSIT-ELT)
+        - Electronics Technology (BSIT-ELX)
+        - Mechanical Technology (BSIT-MT)
+        - Welding and Fabricotion Technology (BSIT-WAFT)
+        - Heating, Ventilation, Air-conditioning and Refrigeration (BSIT-HVACR)
+            
+        - Bachelor of science in Hospitality Manogement (BSHM)
+        - Bachelor of science in Tourism Management (BSTM)
+            
+        BACHELOR OF TECHNICAL- VOCATIONAL TEACHER EDUCATION (BTVTED) MAJOR:
+        - FOOD AND SERVICEs MANAGEMENT
+            
+        BACHELOR OF SECONDARY EDUCATION (BSED) MAJOR IN:
+        - ENGLISH
+        - FILIPINO
+        - MATHEMATICS
+        - SCIENCES
+            
+        - BACHELOR OF ELEMENTARY EDUCATION (BEED)
+        - Bachelor of Physical Education (BPED)
+            
+        - BACHELOR OF SCIENCE IN ENVIRONMENTAL SCIENCE (BSES)
+        - BACHELOR OF SCIENCE IN MATHEMATICS (BSMATH)
+        - BACHELOR OF ARTS IN ENGLISH LANGUAGE (AB-EL)
         ...
         """
     },
@@ -92,6 +129,81 @@ schools = {
         - Master of Public Administration (MPA)
         - Master of Science in Nursing (MSN)
         - Master of Arts in Nursing(MAN)
+        - Master of Arts
+        Major: Science Teaching, Mathematics Teaching, English, Filipino, Home Economics
+        - Master of Arts in Cultural Education (MACulEd)
+        - Master of Arts in Curriculum Development and Design (MA-CDD)
+        - Master of Arts in Educational Management (MAEM)
+            
+        UNDERGRADUATE PROGRAMS
+            
+        COLLEGE OF CULTURE, EDUCATION AND ARTS
+        - Bachelor of Arts
+            
+        Major: Political Sience, Philosophy, English Language, Sociology, Mass Communication
+        - Bachelor of Library and Information Science.
+        - Bachelor of Library and Information Science
+        - Bachelor of Science in Mathematics
+        - Bachelor of Science in Public Administration
+        - Bachelor of Elementary Education
+        - Bachelor of Secondary Education Major in English
+        - Bachelor of Secondary Education Major in Filipino
+        - Bachelor of Secondary Education Major in Mathematics
+        - Bachelor of Secondary Education Major in Sciences
+        - Bachelor of Secondary Education Major in Social Studies
+        - Bachelor of Secondary Education Major in Values Education
+        - Bachelor of Early Childhood Education
+        - Bachelor of Early Physical Education
+            
+        COLLEGE OF BUSINESS AND TECHNOLOGY
+        - Bachelor of Science in Accountancy
+        - Bachelor of Science in Business Administration
+            
+        Major: Financial Management, Marketing Management, Human Resource Development Management
+        - Bachelor of Science in Hospitality Management
+        - Bachelor of Science in Office Administration
+        - Bachelor of Science in Tourism Management
+        - Bachelor of Science in Accounting and Information System
+        - Bachelor of Science in Computer Science
+        - Bachelor of Science in Information Technology
+            
+        COLLEGE OF ENGINEERING
+        - Bachelor of Science in Civil Engineering
+        - Bachelor of Science in Computer Engineering
+        - Bachelor of Science in Mining Engineering
+            
+        COLLEGE OF HEALTH SCIENCES
+        - Bachelor of Science in Nursing
+        - Bachelor of Science in Psychology
+            
+        COLLEGE OF CRIMINAL JUSTICE EDUCATION
+        - Bachelor of Science in Criminology
+        - Bachelor of Science in Forensic Science
+            
+        COLLEGE OF TEACHER EDUCATION
+        - Bachelor in Elementary Education
+        - Bachelor in Secondary Education
+        - Major: English
+        - Filipino
+        - Science
+        - Mathematics
+        - Biological Home Economics
+        - Business Technology
+        - Physical Science
+        - Social Studies
+        - PE., Health and Music
+        - Library and Info Science
+            
+        BASIC EDUCATION
+        - Senior High School (Complete K-12 Curriculum)
+        - Junior High School
+        - Grade School
+            
+        TESDA PROGRAMS
+        - Computer Systems Servicing NCI
+        - Food and Beverage Servicing NCII
+        - Housekeeping NCII
+        - Bookkeeping NCl
         ...
         """
     },
