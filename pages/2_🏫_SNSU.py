92e0621c39ff7f7395cd3f5ff93a433a1ae45046
Jcbituin <150402651+Jcbituin@users.noreply.github.com>
2024-05-22 14:10:18 +0800

    Update 2_🏫_SNSU.py

--- pages/2_🏫_SNSU.py
+++ pages/2_🏫_SNSU.py
@@ -8,7 +8,7 @@ st.title("SURIGAO DEL NORTE STATE UNIVERSITY")
 
 with st.container():
     st.write("---")
-    image_column, text_column = st.columns((5))
+    image_column, text_column = st.columns((5,1))
     with image_column:
         st.image(image1)
         
