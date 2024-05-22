8e26f66c2d7d82a70f53dabe4d9300c698346708
Jcbituin <150402651+Jcbituin@users.noreply.github.com>
2024-05-22 14:36:13 +0800

    Update 2_🏫_SNSU.py

--- pages/2_🏫_SNSU.py
+++ pages/2_🏫_SNSU.py
@@ -3,13 +3,14 @@ import streamlit as st
 from PIL import Image
 
 image1 = Image.open("image/B.png")
-
-st.title("SURIGAO DEL NORTE STATE UNIVERSITY")
 with st.container():
-    st.write("---")
     image_column, text_column = st.columns((2,2))
     with image_column:
         st.image(image1)
+        
+st.write("---")
+st.title("SURIGAO DEL NORTE STATE UNIVERSITY")
+
 import streamlit as st
 
 with st.container():
